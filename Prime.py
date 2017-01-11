0eea15d09c0a76c61bafe07379925363e7c97c63
jeppess <jeppeskov92@gmail.com>
2017-01-11 18:51:49 +0100

    Added few comments

--- Prime.py
+++ Prime.py
@@ -1,8 +1,22 @@
 class Prime(object):
+    """
+    Prime object, represents prime numbers.
+    Used to check if number actually is prime.
+
+    """
+
     __number__ = None 
 
     def __init__(self, number):
+        """
+        Initialize a "prime" number, the objects does not automaticlly check
+        if the number is actually prime.
+
+        Args:
+            number (int): A prime number
+        """
         self.__number__ = number
+
 
     def getNumber(self):
         return self.__number__
