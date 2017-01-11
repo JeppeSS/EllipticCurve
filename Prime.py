0e14b5a77c895c544607226e685958804dc44526
jeppess <jeppeskov92@gmail.com>
2017-01-11 22:22:11 +0100

    Added Prime tester, if time add Miller-Rabin tests also

--- Prime.py
+++ Prime.py
@@ -1,3 +1,47 @@
+import random
+
+
+def gcd(x, y):
+    """ Greatest common divisor, the Euclidean Algorithm 
+    
+    Uses while loop insted of recursion to optimize speed.
+    Python works slow with recursion.
+
+
+    Args:
+        x (int): Positive integer
+        y (int): Positive integer, x >= y
+    """
+
+
+    if (x <= 0) or (y <= 0):
+        raise Exception('Arguments must be positive')
+
+
+    while y != 0:
+        x, y = y, x % y
+
+    return x
+
+
+def fermat(a, p):
+    """ Fermat's little theorem
+
+    If p is a prime number, then for any integer a:
+
+    a ** (p - 1) ≡ 1 mod p
+
+    This is not always the case!
+
+
+    Args:
+        a (int): Number to test
+        p (int): The prime number you want to test 
+    """
+
+    return pow(a, p - 1, p)
+
+
 class Prime(object):
     """
     Prime object, represents prime numbers.
@@ -21,6 +65,32 @@ class Prime(object):
     def getNumber(self):
         return self.__number__
 
+
+
+
+    def isPrime(self, numTest=50):
+        p = self.__number__
+
+        if p == 1:
+            return False
+
+        if numTest >= p:
+            numTest = p - 1
+
+
+        for i in range(numTest):
+            test = random.randint(1, p - 1)
+           
+            print(gcd(test, p))
+            if (gcd(test, p) != 1):
+                return False
+
+            print(fermat(test, p))
+            if fermat(test, p) != 1:
+                return False
+
+        return True
+
+
     def __repr__(self):
         return '[P = %d]' % self.getNumber()
-
