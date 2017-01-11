8da9c88cfb2c7d99ba33a26f25481748f5ee317a
jeppess <jeppeskov92@gmail.com>
2017-01-11 22:27:19 +0100

    Added Point object - represents point on curve

---
+++ Point.py
@@ -0,0 +1,28 @@
+class Point(object):
+    __x__ = None
+    __y__ = None
+
+
+    def __init__(self, x, y):
+        self.__x__ = x
+        self.__y__ = y
+
+
+    def getX(self):
+        return self.__x__
+
+    def getY(self):
+        return self.__y__
+
+    def setX(self, x):
+        self.__x__ = x
+
+    def setY(self, y):
+        self.__y__ = y
+
+    def getCoords(self):
+        return (self.getX(), self.getY())
+
+    def __repr__(self):
+        return '[x = %d, y = %d]' % (self.getX(), self.getY())
+
