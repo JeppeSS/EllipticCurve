f8db226a75222688aa2bcfe8255e0295bb5e40bc
jeppess <jeppeskov92@gmail.com>
2017-01-13 15:45:02 +0100

    Added comment and to get function

--- EllipticCurve.py
+++ EllipticCurve.py
@@ -11,8 +11,20 @@ class EllipticCurve(object):
             raise Exception('The curve is not valid!')
 
 
+    def geta(self):
+        return self.__a__
+
+    def getb(self):
+        return self.__b__
+
     def satisfyCurve(self, a, b):
-        discri = -16 * (((4 * self.__a__) ** 3) + ((27 * self.__b__) ** 2))
+        """ SatisfyCurve is a check that needs to be done to exclude singular
+            curves.
+
+            4a^3 + 27b^2 ≠ 0
+
+        """
+        discri = -16 * (((4 * self.geta()) ** 3) + ((27 * self.getb()) ** 2))
 
         return discri != 0
         
