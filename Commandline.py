36dd404d927b4a9b508e63a81d425681469b7b21
jeppess <jeppeskov92@gmail.com>
2017-01-24 17:18:05 +0100

    Moved argument parser to Commandline.py

--- Commandline.py
+++ Commandline.py
@@ -1,4 +1,7 @@
 import sys
+import CurveDB     as DB
+import PrivateKey  as PK
+import Filemanager as FM
 
 class Commandline(object):
 
@@ -34,9 +37,41 @@ class Commandline(object):
         return self.__commands__
 
 
-
-    def run(self):
+    def run(self, args):
         cmd = self.__getCmd__()
 
         if len(cmd) < 1:
             self.__display_error__("No command supplied.")
+
+        self.parse(args)
+    
+    def parse(self, args):
+        db = DB.CurveDB()
+        fm = FM.Filemanager()
+
+
+        data = ""
+
+
+        if args.list_curves:
+            data = db.printDB()
+        
+        if args.name:
+            curveName = args.name[0]
+            data      = db.printCurve(curveName)
+
+        if args.genkey:
+            pk = PK.PrivateKey()
+
+            pk.generateKey()
+
+            data = pk.printKey()
+
+
+        if args.out:
+            fm.writeOut(data, args.out[0])
+        else:
+            print(data)
+
+        
+    
