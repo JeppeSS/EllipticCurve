d2c73636eca6d987c86d4de8f327c813c61af966
jeppess <jeppeskov92@gmail.com>
2017-01-25 00:10:48 +0100

    Added more commands

--- Commandline.py
+++ Commandline.py
@@ -48,24 +48,53 @@ class Commandline(object):
     def parse(self, args):
         db = DB.CurveDB()
         fm = FM.Filemanager()
+        pk = PK.PrivateKey()
+        
 
+        cmd = self.__getCmd__()
 
         data = ""
 
-
-        if args.list_curves:
-            data = db.printDB()
         
-        if args.name:
-            curveName = args.name[0]
-            data      = db.printCurve(curveName)
+        if cmd[0] == 'ecparam':
+        
+            if args.list_curves:
+                data = db.printDB()
+            
+            if args.name:
+                curveName = args.name[0]
+                data      = db.printCurve(curveName)
 
-        if args.genkey:
-            pk = PK.PrivateKey()
+            if args.genkey:
 
-            pk.generateKey()
+                pk.generateKey()
 
-            data = pk.printKey()
+                data = pk.printKey()
+        
+        if cmd[0] == 'encrypt':
+            if args.name:
+                curveName = args.name[0]
+                curve     = db.getCurve(curveName)
+
+                if curve == 0:
+                    self.__display_error__("Invalid Curve")
+
+                a     = curve[2]
+                b     = curve[3]
+                prime = curve[4]
+                gX    = curve[5][0]
+                gY    = curve[5][1]
+            
+            else:
+                self.__display_error__("Choose a curve -name 'short name'")
+
+            if args.key:
+                keyFile = fm.readData(args.key[0])
+                key     = pk.extractKey(keyFile)
+
+            else:
+                self.__display_error__("Select a key file -key 'filename'")
+
 
 
         if args.out:
