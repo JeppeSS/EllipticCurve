e6da94657f535f2f5e87f94b40dd51d365fadda8
jeppess <jeppeskov92@gmail.com>
2017-01-24 23:26:52 +0100

    Code cleanup

--- PrivateKey.py
+++ PrivateKey.py
@@ -3,23 +3,13 @@ import base64
 import Filemanager as FM
 
 class PrivateKey(object):
-    __key__ = None
-
-
 
     def generateKey(self, size = 256):
         key = os.urandom(size)
         key = base64.b64encode(key)
-        self.__key__ = key
     
 
         return key
-
-
-
-    def getKey(self):
-        return self.__key__
-
 
 
     def extractKey(self,key):
