ad77027a92e6452b4cc3c058ef11e356049cf734
jeppess <jeppeskov92@gmail.com>
2017-01-24 23:24:58 +0100

    Extract privatekey

--- PrivateKey.py
+++ PrivateKey.py
@@ -1,5 +1,6 @@
 import os
 import base64
+import Filemanager as FM
 
 class PrivateKey(object):
     __key__ = None
@@ -9,8 +10,8 @@ class PrivateKey(object):
     def generateKey(self, size = 256):
         key = os.urandom(size)
         key = base64.b64encode(key)
-
         self.__key__ = key
+    
 
         return key
 
@@ -20,16 +21,23 @@ class PrivateKey(object):
         return self.__key__
 
 
+
+    def extractKey(self,key):
+        key = key.decode('utf-8')
+        key = key.splitlines()
+        key = base64.b64decode(key[1])
+        key = int.from_bytes(key, byteorder='big')
+        
+        return key
+
+
     def printKey(self):
         key = self.getKey()
 
 
-        keyparse = '-----BEGIN EC PRIVATE KEY-----\n{}\n-----END EC PRIVATE KEY-----'.format(key.decode('utf8'))
+        keyparse  = '-----BEGIN EC PRIVATE KEY-----\n'
+        keyparse += '{}\n-----END EC PRIVATE KEY-----'.format(key.decode('utf8'))
 
 
         return keyparse
-
-
-
-
 
