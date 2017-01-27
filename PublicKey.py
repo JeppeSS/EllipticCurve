fda89bde44a1cfef6f2b123f8d3688f19e932cae
jeppess <jeppeskov92@gmail.com>
2017-01-27 17:52:27 +0100

    Changed to publickey

---
+++ PublicKey.py
@@ -0,0 +1,30 @@
+import os
+import base64
+
+class PublicKey(object):
+
+    def generateKey(self, size = 256):
+        key = os.urandom(size)
+        key = base64.b64encode(key)
+    
+
+        return key
+
+
+    def extractKey(self,key):
+        key = key.decode('utf-8')
+        key = key.splitlines()
+        key = base64.b64decode(key[1])
+        key = int.from_bytes(key, byteorder='big')
+        
+        return key
+
+
+    def printKey(self, key):
+
+        keyparse = '-----BEGIN EC PUBLIC KEY-----\n'
+        keyparse += '{}\n-----END EC PUBLIC KEY-----'.format(key.decode('utf8'))
+
+
+        return keyparse
+
